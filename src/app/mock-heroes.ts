9b8adbc998dadcaaa36afcb4d8743d0f6fb6d018
linkun-wang <linkun0922@163.com>
2019-03-24 19:52:17 +0800

    修改mock数据id.

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-    { id: 11, name: '艾希-Ashe'},
-    { id: 12, name: '瑞兹-Ryze'},
-    { id: 13, name: '盖伦-Garen'},
-    { id: 14, name: '嘉文四世-JarvanIV'},
-    { id: 15, name: '拉克丝-Lux'},
-    { id: 16, name: '墨菲特-Malphite'},
-    { id: 17, name: '易-Yi'},
-    { id: 18, name: '孙悟空-Monkey king'},
-    { id: 19, name: '李青-Lee sin'},
-    { id: 20, name: '赵信-XinZhao'},
+    { id: 1, name: '艾希-Ashe'},
+    { id: 2, name: '瑞兹-Ryze'},
+    { id: 3, name: '盖伦-Garen'},
+    { id: 4, name: '嘉文四世-JarvanIV'},
+    { id: 5, name: '拉克丝-Lux'},
+    { id: 6, name: '墨菲特-Malphite'},
+    { id: 7, name: '易-Yi'},
+    { id: 8, name: '孙悟空-Monkey king'},
+    { id: 9, name: '李青-Lee sin'},
+    { id: 10, name: '赵信-XinZhao'},
 ];
